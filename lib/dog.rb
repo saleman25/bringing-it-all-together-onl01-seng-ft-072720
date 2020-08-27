f801e0e2cf80cef84e1b36b5068ac6f03cae72eb
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-27 21:33:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,7 +24,17 @@ def self.drop_table
 end 
 
 def save 
-  
+  if self.id
+     self.update
+  else
+    sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+  SQL
+  DB[:conn].execute(sql, self.name, self.breed)
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+end
+  self
 end 
 
 def self.create 
