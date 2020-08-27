f3bafb235e55257db06e18241c3f38c2837bd761
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-27 21:19:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,9 @@ class Dog
   
 attr_accessor :name, :breed, :id 
 
-def initialize(name, breed, id= nil)
+def initialize(id= nil, name, breed)
   @name = name 
   @breed = breed 
-  @id = id 
 end 
 
 def self.create_table 
