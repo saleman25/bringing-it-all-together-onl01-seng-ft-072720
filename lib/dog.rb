b84669d8b8ebf3a3253d8a8a5a4a4dbb4dd77ba6
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-27 21:40:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,7 +47,24 @@ def self.find_by_id
 end 
 
 def self.find_or_create_by
-  
+  sql = <<-SQL
+          SELECT *
+          FROM dogs
+          WHERE name = ?
+          AND breed = ?
+          LIMIT 1
+        SQL
+
+  dog = DB[:conn].execute(sql,name,breed)
+
+  if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+  else
+      dog = self.create(name: name, breed: breed)
+end
+dog
+end
 end 
 
 def self.find_by_name 
